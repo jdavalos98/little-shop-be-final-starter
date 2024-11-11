d8070eb544edf2d7043d6bcbd29142ffd7c4dc20
Joel Davalos <jsdavalos@gmail.com>
2024-11-10 18:47:04 -0700

    Added tests for show endpoint.

---
+++ spec/requests/api/v1/merchant/coupon_request_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe 'Coupon endpoints' do
+  before(:each) do
+    @merchant = create(:merchant)
+    @merchant2 = create(:merchant)
+    @coupon = create(:coupon, merchant: @merchant2, name: 'Buy One Get One 50')
+    @coupons = create_list(:coupon, 5, merchant: @merchant, active: true)
+    @inactive_coupon = create(:coupon, merchant: @merchant, active: false)
+    @invoice_1 = create(:invoice, coupon: @coupon)
+    @invoice_2 = create(:invoice, coupon: @coupon)
+    @invoice_3 = create(:invoice)
+  end
+
+  it 'returns a specific coupon with usage count' do
+    get "/api/v1/merchants/#{@merchant2.id}/coupons/#{@coupon.id}"
+
+    expect(response).to be_sucessful
+    expect(response).to have_http_status(200)
+
+    json_response = JSON.parse(response.body, symbolize_names: true)
+    data = json_response[:data]
+
+    expect(data).to have_key(:id)
+    expect(data[:id]).to eq(@coupon.id.to_s)
+
+    expect(data).to have_key(:type)
+    expect(data[:type]).to eq('coupon')
+
+    expect(data[:attributes]).to be_a(Hash)
+    attributes = dta[:attributes]
+
+    expect(attributes[:name]).to eq('Buy One Get One 50')
+    exepct(attributes[:code]).to eq(@coupon.code)
+    exepct(attributes[:discount_type]).to eq(@coupon.discount_type)
+    exepct(attributes[:discount_value]).to eq(@coupon.discount_value)
+    expect(attributes[:active]).to eq(@coupon.active)
+    expect(attributes[:usage_count]).to eq(2)
+  end
+end
