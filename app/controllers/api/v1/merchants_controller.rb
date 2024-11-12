8dd62047db6e6ff41e2fa977ff0d3b3ca1d894e5
Joel Davalos <jsdavalos@gmail.com>
2024-11-12 14:09:46 -0700

    updated index action to take in counts for invoice and coupons

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -2,15 +2,16 @@ class Api::V1::MerchantsController < ApplicationController
 
   def index
     merchants = Merchant.all
-
+  
     if params[:sorted].present? && params[:sorted] == "age"
       merchants = merchants.sorted_by_creation
     elsif params[:status].present?
       merchants = Merchant.filter_by_status(params[:status])
     end
-
-    include_count = params[:count].present? && params[:count] == "true"
-    render json: MerchantSerializer.new(merchants, { params: { count: include_count }})
+  
+    include_counts = params[:count].present? && params[:count] == "true"
+    
+    render json: MerchantSerializer.new(merchants, { params: { include_counts: include_counts } })
   end
 
   def show
