96c7dca7962f6e81c1698492b2fe5381fe020f00
Joel Davalos <jsdavalos@gmail.com>
2024-11-12 18:34:19 -0700

    updated tests for invoice model to up test coverage

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -1,7 +1,14 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe Invoice do
-  it { should belong_to :merchant }
-  it { should belong_to :customer }
-  it { should validate_inclusion_of(:status).in_array(%w(shipped packaged returned)) }
+RSpec.describe Invoice, type: :model do
+  describe "associations" do
+    it { should belong_to(:merchant) }
+    it { should belong_to(:customer) }
+    it { should belong_to(:coupon).optional }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:status) }
+    it { should validate_inclusion_of(:status).in_array(%w[pending shipped canceled]) }
+  end
 end
